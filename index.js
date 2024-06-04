ca46ea669c1ce25111c75fc4f73c2335f74c43e5
sandroex <sandroex_compras@hotmail.com>
2024-06-04 12:18:37 -0300

    basic project

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app
+    .get('/', (req, res) => {
+    res.send('Teste pipe')
+    })
+    .listen(port, () => {console.log('Server On-line')})
